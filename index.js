72fc932ad7e6a81faa9e6f1abd93a36a1cf67837
mahedi <mahedianik4@gmail.com>
2022-05-12 02:32:11 +0600

    create all service get api

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
-const { MongoClient, ServerApiVersion } = require('mongodb');
+
 const express = require('express')
 const cors = require('cors');
 const app = express()
 const port = process.env.PORT || 5000
 require('dotenv').config()
-// middle ware
-app.use(cors)
-app.use(express.json)
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
+//middleware
+app.use(cors())
+app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qyjpo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
@@ -28,8 +29,6 @@ async function run() {
 }
 }
 run().catch(console.dir);
-
-
 app.get('/', (req, res) => {
   res.send('dr- portal server is running !')
 })
